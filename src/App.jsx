d9beaaac6bde077d4229948a9cd46a12e226b73f
Deyvison Tavares <deyvisontav@gmail.com>
2022-06-14 19:07:35 -0300

    add button svg

--- src/App.jsx
+++ src/App.jsx
@@ -18,12 +18,12 @@ export function App(){
       
        
     <div className="h-32">
-       <div className="pt-16 flex items-end justify-end text-green-600 p-5 text-2xl cursor-pointer">
+       <button className="pt-16 flex items-end justify-end text-green-600 p-5 text-2xl cursor-pointer">
        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
   <path stroke-linecap="round" stroke-linejoin="round" d="M12 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2M3 12l6.414 6.414a2 2 0 001.414.586H19a2 2 0 002-2V7a2 2 0 00-2-2h-8.172a2 2 0 00-1.414.586L3 12z" />
 </svg>
            
-      </div>       
+      </button>       
      
     </div>
       
